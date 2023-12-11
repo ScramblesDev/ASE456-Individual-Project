aa452f09bcc1868d339c8c350fb600feb03b950c
ScramblesDev <beesley02@gmail.com>
2023-12-10 21:50:09 -0500

    updated code

--- documents/requirements/user_stories_v2.md
+++ documents/requirements/user_stories_v2.md
@@ -1,16 +1,13 @@
 # Requirements (User Stories) - ver. 2
 
 ## User Story 1
-As a user who values productivity, I want the ability to customize the duration of my work and break intervals within the Pomodoro app. This customization should be easily accessible, either through a dedicated settings page or a prominent button on the timer interface. By having the option to set my preferred duration, I can tailor the app to match my specific workflow and concentration levels.
+As a user who values productivity, I want the ability to manage the duration of my work and break intervals within the Pomodoro app. This customization should be easily accessible, either through a dedicated settings page or a prominent button on the timer interface. By having the option to set my preferred duration, I can tailor the app to match my specific workflow and concentration levels.
 
 ## User Story 2
-As a user committed to tracking my work habits and progress, I seek the functionality to create a personal account within the Pomodoro app. This account should allow me to save and log my recorded Pomodoro sessions, providing a comprehensive record of my work history. This feature will empower me to monitor my productivity over time, set goals, and assess my performance, ultimately helping me become more organized and efficient in my work.
+As a use who appreciates being able to see progress, I want to have a progress bar feature to visually track my work and break intervals so that I can easily see how much time is left in my current session and stay motivated to stay on task. The progress bar should accurately reflect the time remaining in the current Pomodoro session or break period.
 
 ## User Story 3
-As a user with sensitivity to screen brightness and color, I value the option to switch between dark and light modes within the Pomodoro app. This feature should seamlessly transition the app's background and text color scheme based on my preference, which I can easily toggle with a dedicated button. The ability to personalize the app's appearance contributes to a more enjoyable and accommodating user experience.
-
-## User Story 4
 As a user who appreciates personalization and aesthetics, I desire the capability to customize the visual theme of the Pomodoro app by selecting my preferred image of a fruit or vegetable. This choice should persist consistently throughout my interaction with the application, from the initial screen to the timer display. Such visual personalization enhances my overall user experience and makes the app feel uniquely mine.
 
-## User Story 5
+## User Story 4
 As a user who relies on auditory cues for time management, I require the Pomodoro app to incorporate audible notifications into its timer functionality. Specifically, I expect the app to emit distinct and pleasant sounds to signal the beginning and end of each work and break interval. These auditory cues should be sufficiently loud to capture my attention without being jarring, contributing to a productive and harmonious work environment.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(PomodoroApp());
 
@@ -7,7 +8,7 @@ class PomodoroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Pomodoro Timer',
+      title: 'Pomodoro Timer',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
@@ -25,6 +26,35 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
   static const duration = Duration(minutes: 25);
   Timer? timer;
   Duration remaining = duration;
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  // List of fruit and vegetable images
+  final List<String> images = [
+    'tomato.png',
+    'pear.png',
+    'apple.png',
+    'lemon.png',
+    'orange.png',
+    'cucumber.png'
+  ];
+  int currentImageIndex = 0;
+
+  Color getBackgroundColor() {
+    switch (images[currentImageIndex]) {
+      case 'tomato.png':
+      case 'apple.png':
+        return Color.fromRGBO(255, 182, 193, 0.3); // Light Pink
+      case 'cucumber.png':
+      case 'pear.png':
+        return Color.fromRGBO(144, 238, 144, 0.3); // Light Green
+      case 'lemon.png':
+        return Color.fromRGBO(255, 247, 153, 0.3); // Light Yellow
+      case 'orange.png':
+        return Color.fromRGBO(255, 165, 0, 0.2); // Light Orange
+      default:
+        return Colors.white; // Default color
+    }
+  }
 
   void startTimer() {
     if (timer == null || !timer!.isActive) {
@@ -33,12 +63,17 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
           final seconds = remaining.inSeconds - 1;
           if (seconds < 0) {
             timer?.cancel();
+            playAlarm();
           } else {
             remaining = Duration(seconds: seconds);
           }
         });
       });
     }
+  }
+
+  void playAlarm() async {
+    await audioPlayer.play(AssetSource('ring.mp3'));
   }
 
   void stopTimer() {
@@ -48,27 +83,74 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
     });
   }
 
+  void changeImage() {
+    setState(() {
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double progressBarWidth = screenWidth * 0.5;
+    double filledBarWidth =
+        (remaining.inSeconds / duration.inSeconds) * progressBarWidth;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Simple Pomodoro Timer'),
       ),
+      backgroundColor: getBackgroundColor(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '${remaining.inMinutes}:${(remaining.inSeconds % 60).toString().padLeft(2, '0')}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            ElevatedButton(
-              onPressed: startTimer,
-              child: Text('Start'),
-            ),
-            ElevatedButton(
-              onPressed: stopTimer,
-              child: Text('Reset'),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(images[currentImageIndex]),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: changeImage,
+                  child: Text('Change Fruit!'),
+                ),
+                Text(
+                  '${remaining.inMinutes}:${(remaining.inSeconds % 60).toString().padLeft(2, '0')}',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4
+                      ?.copyWith(color: Colors.black),
+                ),
+                ElevatedButton(
+                  onPressed: startTimer,
+                  child: Text('Start'),
+                ),
+                ElevatedButton(
+                  onPressed: stopTimer,
+                  child: Text('Reset'),
+                ),
+                SizedBox(height: 20), // Spacing
+                // The loading bar
+                Container(
+                  width: progressBarWidth,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: Colors
+                        .grey[300], // Background color of the progress bar
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    width: filledBarWidth,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: Colors.white, // Filled part color
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20), // Additional spacing
+                // ... other widgets ...
+              ],
             ),
           ],
         ),
